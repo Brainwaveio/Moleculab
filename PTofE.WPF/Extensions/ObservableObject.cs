b57dc9e1d0023a11fc91bc295712b73f822195b9
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-04-20 23:58:18 +0300

    update WPF

---
+++ PTofE.WPF/Extensions/ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PTofE.WPF.Extensions
+{
+	internal class ObservableObject : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
