6164507ab220b0a38dd7f1d0477cb01657701541
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-04-27 18:49:19 +0300

    Update Program.cs

--- QuantumQuery.Console/Program.cs
+++ QuantumQuery.Console/Program.cs
@@ -6,18 +6,18 @@ public class Program
 {
 	private static async Task Main(string[] args)
 	{
-		var mapperConfig = MapperConfig.RegisterMaps();
-		var mapper = mapperConfig.CreateMapper();
+		//var mapperConfig = MapperConfig.RegisterMaps();
+		//var mapper = mapperConfig.CreateMapper();
 
 		DotNetEnv.Env.Load();
 		Console.WriteLine("Hello, World!");
 
 		//DatabaseInitializer.InitializeDatabase();
 
-		//var test = new JsonConverter();
-		//await test.ConvertToJsonFileAsync(
-		//	"F:\\Projects\\src\\PeriodicTableOfElements\\PTofE.Console\\Input\\PubChemElements_all.json",
-		//	"F:\\Projects\\src\\PeriodicTableOfElements\\PTofE.Console\\Output\\PubChemElements_all.json");
+		var test = new JsonConverter();
+		await test.ConvertToJsonFileAsync(
+			"F:\\Projects\\src\\QuantumQuery\\QuantumQuery.Console\\Input\\PubChemElements_all.json",
+			"F:\\Projects\\src\\QuantumQuery\\QuantumQuery.Console\\Output\\PubChemElements_all.json");
 
 		Console.ReadLine();
 	}
