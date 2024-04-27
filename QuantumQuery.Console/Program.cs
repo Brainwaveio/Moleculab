53eec1169ed438b64a58a1e71964d2626a9091dd
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-04-27 18:44:23 +0300

    Update Program.cs

--- QuantumQuery.Console/Program.cs
+++ QuantumQuery.Console/Program.cs
@@ -1,5 +1,4 @@
 ﻿using QuantumQuery.Core.JSONConvertor;
-using QuantumQuery.Core.Services;
 using QuantumQuery.Core.SQLite;
 using QuantumQuery.DAL.SQLite;
 
@@ -13,61 +12,7 @@ public class Program
 		DotNetEnv.Env.Load();
 		Console.WriteLine("Hello, World!");
 
-		DatabaseInitializer.InitializeDatabase();
-
-		////var tesst = Environment.CurrentDirectory;
-
-		//var t = new ElementService();
-
-		//var te = await t.GetAllAsync();
-
-		//foreach (var item in te)
-		//{
-		//	Console.WriteLine(item.Index);
-		//	Console.WriteLine(item.ShortName);
-		//}
-
-		//Console.WriteLine(te);
-
-		//string databasePath = "myTestDb.db";
-		//var elementService = new QuantumQuery.Core.LiteDB.Services.TestElementService(databasePath);
-
-		//// Створення нового елемента
-		//var newElement = new Element
-		//{
-		//	Id = Guid.NewGuid(),
-		//	ElementName = "Neon",
-		//	AtomicMass = 20.180f,
-		//	CPKHexColor = "dfsgf",
-		//	GroupBlock = "ds",
-		//	Index = 1,
-		//	ShortName = "H",
-		//};
-
-		//// Вставка елемента
-		//await elementService.Insert(newElement);
-		//Console.WriteLine($"Element inserted: {newElement.ElementName}");
-
-		//// Оновлення елемента
-		//newElement.ElementName = "Modified Neon";
-		//await elementService.Update(newElement);
-		//Console.WriteLine($"Element updated: {newElement.ElementName}");
-
-		//// Отримання елемента
-		//var element = await elementService.GetById(newElement.Id);
-		//Console.WriteLine($"Retrieved Element: {element.ElementName}");
-
-		//// Видалення елемента
-		//await elementService.Delete("");
-		//Console.WriteLine("Element deleted");
-
-		//// Отримання всіх елементів
-		//var allElements = await elementService.GetAll();
-		//Console.WriteLine("All elements:");
-		//foreach (var elem in allElements)
-		//{
-		//	Console.WriteLine($"Element: {elem.ElementName}");
-		//}
+		//DatabaseInitializer.InitializeDatabase();
 
 		//var test = new JsonConverter();
 		//await test.ConvertToJsonFileAsync(
