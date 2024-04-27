7223ccc404764ce1c0b6987afb442c9554460eda
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-04-27 12:50:12 +0300

    Create DirectoryExtensions.cs

---
+++ QuantumQuery.Core/Extensions/DirectoryExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace QuantumQuery.Core.Extensions
+{
+	public static class DirectoryExtensions
+	{
+		public static DirectoryInfo? GetRootDirectory(int fromDirectory = 3)
+		{
+			var directory = Directory.GetParent(Environment.CurrentDirectory);
+			for (var i = 0; i < fromDirectory;  i++)
+			{
+				directory = Directory.GetParent(directory?.FullName
+					?? throw new ArgumentNullException("Directory path can not be null"));
+			}
+			return directory;
+		}
+	}
+}
