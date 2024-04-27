51b1c8f447beea80adde327a47cf01d3daffb983
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-04-27 17:58:54 +0300

    add db context

---
+++ QuantumQuery.DAL.SQLite/Context/QuantumQueryDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using QuantumQuery.DAL.SQLite.Models;
+
+namespace QuantumQuery.DAL.SQLite.Context;
+
+public partial class QuantumQueryDbContext : DbContext
+{
+    public QuantumQueryDbContext(DbContextOptions<QuantumQueryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Element> Elements { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
