2bf46b68df71ed01a3f725643cd9bbe6b9bcc5c7
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-05-08 16:00:00 +0300

    Update Program.cs

--- Moleculab.Console/Program.cs
+++ Moleculab.Console/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Moleculab.Core.JSONConvertor;
+using Moleculab.Core.Services;
 using Moleculab.Core.SQLite;
+using Moleculab.Core.SQLite.DTOs;
 using Moleculab.DAL.SQLite;
+using Newtonsoft.Json;
 
 public class Program
 {
@@ -14,10 +17,23 @@ public class Program
 
 		//DatabaseInitializer.InitializeDatabase();
 
-		var test = new JsonConverter();
-		await test.ConvertToJsonFileAsync(
-			"F:\\Projects\\src\\QuantumQuery\\QuantumQuery.Console\\Input\\PubChemElements_all.json",
-			"F:\\Projects\\src\\QuantumQuery\\QuantumQuery.Console\\Output\\PubChemElements_all.json");
+		//var jsonConverter = new JsonConverter();
+		//await jsonConverter.ConvertToJsonFileAsync(
+		//	"F:\\Projects\\src\\QuantumQuery\\QuantumQuery.Console\\Input\\PubChemElements_all.json",
+		//	"F:\\Projects\\src\\QuantumQuery\\QuantumQuery.Console\\Output\\PubChemElements_all.json");
+
+		var jsonElementService = new JSONElementService();
+		var elementFromJson = await jsonElementService.GetAllAsync();
+
+		var elements = new List<string>();
+
+		foreach (var element in elementFromJson)
+		{
+			elements.Add($"INSERT INTO Element ([Id], [Index], [ShortName], [ElementName], [AtomicMass], [CPKHexColor], [StandardState], [ElectronConfiguration], [OxidationStates], [Electronegativity], [AtomicRadius], [IonizationEnergy], [ElectronAffinity], [MeltingPoint], [BoilingPoint], [Density], [GroupBlock], [YearDiscovered]) VALUES ('{element.Id}', {element.Index}, '{element.ShortName}', '{element.ElementName}', {element.AtomicMass}, '{element.CpkhexColor}', '{element.StandardState}', '{element.ElectronConfiguration}', '{element.OxidationStates}', {element.Electronegativity}, {element.AtomicRadius}, {element.IonizationEnergy}, {element.ElectronAffinity}, {element.MeltingPoint}, {element.BoilingPoint}, {element.Density}, '{element.GroupBlock}', '{element.YearDiscovered}');");
+		}
+
+		var outputSql = string.Join("\n", elements);
+		await File.WriteAllTextAsync(@"F:\Projects\src\Moleculab\SQL\Data\ElementData.sql", outputSql);
 
 		Console.ReadLine();
 	}
