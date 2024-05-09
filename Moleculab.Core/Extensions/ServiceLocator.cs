d11c0c5841734c321fdeb046c2de83e41c76fe1a
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-05-09 11:45:32 +0300

    Create ServiceLocator.cs

---
+++ Moleculab.Core/Extensions/ServiceLocator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Moleculab.Core.Extensions
+{
+	public class ServiceLocator
+	{
+		private static IServiceProvider _serviceProvider;
+
+		public static void SetServiceProvider(IServiceProvider serviceProvider)
+		{
+			_serviceProvider = serviceProvider;
+		}
+
+		public static T GetService<T>()
+		{
+			return _serviceProvider.GetRequiredService<T>();
+		}
+	}
+}
