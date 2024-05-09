a6fcc987969e758cfc590d48373685fb7acdf9a9
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-05-09 11:46:27 +0300

    Create IService.cs

---
+++ Moleculab.Core.SQLite/Interfaces/BaseInterfaces/IService.cs
@@ -0,0 +1,14 @@
+﻿using Moleculab.Core.SQLite.DTOs;
+
+namespace Moleculab.Core.SQLite.Interfaces.BaseInterfaces
+{
+    public interface IService<TRequest, TResponse>
+        where TRequest : class
+        where TResponse : class
+    {
+        Task<TRequest> GetById(Guid id);
+        Task<List<TRequest>> GetAll();
+        Task<TRequest> UpdateOrInsert(TResponse obj);
+        Task<DeleteDto> DeleteById(Guid id);
+    }
+}
