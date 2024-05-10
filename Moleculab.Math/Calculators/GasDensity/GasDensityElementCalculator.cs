1cf680484ab80d7d6433e4ebd15a25c9b2f79c2c
Nazar Kacharaba <nazarr.kacharaba@gmail.com>
2024-05-10 20:58:57 +0300

    add GasDensityElementCalculator

---
+++ Moleculab.Math/Calculators/GasDensity/GasDensityElementCalculator.cs
@@ -0,0 +1,91 @@
+﻿using Moleculab.Core.Extensions;
+using Moleculab.Core.SQLite.DTOs;
+using Moleculab.Core.SQLite.Interfaces;
+using Moleculab.Math.Interfaces.Calculators.GasDensity;
+
+namespace Moleculab.Math.Calculators.GasDensity
+{
+    public class GasDensityElementCalculator : IGasDensityElementCalculator
+	{
+		private readonly IElementService _elementService;
+
+		private ElementDto _element;
+		private int _quantity;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+		public GasDensityElementCalculator()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+		{
+			_elementService = ServiceLocator.GetService<IElementService>();
+		}
+
+		public GasDensityElementCalculator(ElementDto element, int quantity)
+		{
+			_element = element;
+			_quantity = quantity;
+			_elementService = ServiceLocator.GetService<IElementService>();
+		}
+
+		public async Task AddDensityOfElementAsync(Element element, int quantity)
+		{
+			try
+			{
+				var sqlElement = await _elementService.GetByShortNameAsync(element.ToString());
+
+				_element = sqlElement;
+				_quantity = quantity;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		public async Task<float> GetEqualsAsync(Element element)
+		{
+			try
+			{
+				var sqlElement = await _elementService.GetByShortNameAsync(element.ToString());
+				var atomicMassOfDelement = float.NaN;
+
+				if (_element.ShortName == Element.Cl.ToString())
+				{
+					atomicMassOfDelement = (float)35.35 * _quantity;
+				}
+				else
+				{
+					atomicMassOfDelement = (int)System.Math.Round(_element.AtomicMass) * _quantity;
+				}
+
+				return (float)System.Math.Round(sqlElement.AtomicMass) / atomicMassOfDelement;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		public async Task<ElementDto> GetElementAsync()
+		{
+			try
+			{
+				var atomicMassOfElement = float.NaN;
+
+				if (_element.ShortName == Element.Cl.ToString())
+				{
+					atomicMassOfElement = (float)35.35	* _quantity;
+				}
+				else
+				{
+					atomicMassOfElement = (int)System.Math.Round(_element.AtomicMass) * _quantity;
+				}
+
+				return await _elementService.GetByAtomicMassAsync((int)atomicMassOfElement);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+	}
+}
---
+++ Moleculab.Math/Interfaces/Calculators/GasDensity/IGasDensityElementCalculator.cs
@@ -0,0 +1,11 @@
+﻿using Moleculab.Core.SQLite.DTOs;
+
+namespace Moleculab.Math.Interfaces.Calculators.GasDensity
+{
+    public interface IGasDensityElementCalculator
+    {
+        Task AddDensityOfElementAsync(Element element, int quantity);
+        Task<float> GetEqualsAsync(Element element);
+        Task<ElementDto> GetElementAsync();
+    }
+}
